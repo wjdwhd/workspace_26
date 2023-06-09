302e9dcbdab95287ee143fba4d926960076e73ff
wjdwhd <whda3923@gmail.com>
2023-06-09 17:44:51 +0900

    study

---
+++ 230607/first.py
@@ -0,0 +1,7 @@
+# 아나콘다랑 파이참 설치 처음으로 실행한 문구
+print("hello_world")
+
+# aws toolkit을 설치 후 developer tools가 잘되는 지 확인한 문구
+a = 1
+b = 2
+print(a+b)
---
+++ 230608/backjun_test/test1.py
@@ -0,0 +1,2 @@
+A, B = map(int, input().split()) # 표준 입력
+print (A / B)
